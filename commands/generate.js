5bb9c2587f9610d4869bd1d6610bfd0a905b1ff5
yessine-rebhi <rabhiyassin@gmail.com>
2024-12-22 01:45:27 +0100

    beta version

--- commands/generate.js
+++
@@ -1,6 +0,0 @@
-import { generateEnvExampleFromCode } from "../src/services/generateEnvExampleFromCode.js";
-
-export const runGenerate = () => {
-  console.log('🔍 Running "generate" command...');
-  generateEnvExampleFromCode();
-};
